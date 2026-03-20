15b2e900c333b90a2caf956c1cbf0c0441b365c6
Zeping Bai <zeping@api7.ai>
2026-03-20 16:51:52 +0800

    test: add more server e2e cases

--- tests/e2e/tests/server.test.ts
+++ tests/e2e/tests/server.test.ts
@@ -1,8 +1,21 @@
+import { spawn } from 'child_process';
+
 import { client } from '../utils/http.js';
-import { App, AppConfig, defaultConfig, randomPort } from '../utils/setup.js';
+import {
+  App,
+  AppConfig,
+  ERR_UNEXPECTED_EARLY_EXIT,
+  ERR_UNEXPECTED_EXIT,
+  defaultConfig,
+  randomPort,
+} from '../utils/setup.js';
+
+const INVALID_LISTEN_ADDR = 'invalid-listen-addr';
 
 describe('proxy server', () => {
-  let server: App;
+  const expectedStatus = 401;
+
+  let server: App | undefined;
 
   afterEach(() => server?.exit());
 
@@ -10,7 +23,7 @@ describe('proxy server', () => {
     server = await (await App.spawn()).waitForReady();
 
     const resp = await client.get(`http://localhost:3000/`);
-    expect(resp.status).toBe(401);
+    expect(resp.status).toBe(expectedStatus);
   });
 
   test('listen http (specify addr)', async () => {
@@ -20,12 +33,54 @@ describe('proxy server', () => {
     ).waitForReady(port);
 
     const resp = await client.get(`http://localhost:${port}/`);
-    expect(resp.status).toBe(401);
+    expect(resp.status).toBe(expectedStatus);
+  });
+
+  test('empty listen', async () => {
+    server = await (
+      await App.spawn(defaultConfig({ listen: undefined }))
+    ).waitForReady(3000);
+
+    const resp = await client.get(`http://localhost:3000/`);
+    expect(resp.status).toBe(expectedStatus);
+  });
+
+  test('invalid listen', async () => {
+    server = undefined;
+
+    await expect(
+      App.spawn(defaultConfig({ listen: INVALID_LISTEN_ADDR }), 1000),
+    ).rejects.toThrow(ERR_UNEXPECTED_EARLY_EXIT);
+  });
+
+  test('port in use', async () => {
+    server = await (
+      await App.spawn(
+        defaultConfig({
+          // avoid conflict with admin server
+          deployment: { admin: { listen: '127.0.0.1:30000' } },
+        }),
+      )
+    ).waitForReady(3000);
+
+    const resp = await client.get(`http://localhost:3000/`);
+    expect(resp.status).toBe(expectedStatus);
+
+    await expect(
+      App.spawn(
+        defaultConfig({
+          deployment: { admin: { listen: '127.0.0.1:30001' } },
+        }),
+        1000,
+      ),
+    ).rejects.toThrow(ERR_UNEXPECTED_EARLY_EXIT);
   });
 });
 
 describe('admin server', () => {
-  let server: App;
+  const expectedStatus = 404;
+
+  let server: App | undefined;
 
   afterEach(() => server?.exit());
 
@@ -33,7 +88,7 @@ describe('admin server', () => {
     server = await (await App.spawn()).waitForReady(3001);
 
     const resp = await client.get(`http://localhost:3001/`);
-    expect(resp.status).toBe(404);
+    expect(resp.status).toBe(expectedStatus);
   });
 
   test('listen http (specify addr)', async () => {
@@ -47,6 +102,48 @@ describe('admin server', () => {
     ).waitForReady(port);
 
     const resp = await client.get(`http://localhost:${port}/`);
-    expect(resp.status).toBe(404);
+    expect(resp.status).toBe(expectedStatus);
+  });
+
+  test('empty listen', async () => {
+    server = await (
+      await App.spawn(
+        defaultConfig({ deployment: { admin: { listen: undefined } } }),
+      )
+    ).waitForReady(3001);
+
+    const resp = await client.get(`http://localhost:3001/`);
+    expect(resp.status).toBe(expectedStatus);
+  });
+
+  test('invalid listen', async () => {
+    server = undefined;
+
+    await expect(
+      App.spawn(
+        defaultConfig({
+          deployment: { admin: { listen: INVALID_LISTEN_ADDR } },
+        }),
+        1000,
+      ),
+    ).rejects.toThrow(ERR_UNEXPECTED_EARLY_EXIT);
+  });
+
+  test('port in use', async () => {
+    server = await (
+      await App.spawn(
+        defaultConfig(
+          // avoid conflict with proxy server
+          { listen: '127.0.0.1:30000' },
+        ),
+      )
+    ).waitForReady(3001);
+
+    const resp = await client.get(`http://localhost:3001/`);
+    expect(resp.status).toBe(expectedStatus);
+
+    await expect(
+      App.spawn(defaultConfig({ listen: '127.0.0.1:30001' }), 1000),
+    ).rejects.toThrow(ERR_UNEXPECTED_EARLY_EXIT);
   });
 });
--- tests/e2e/utils/setup.ts
+++ tests/e2e/utils/setup.ts
@@ -38,13 +38,27 @@ export const defaultConfig = (overrides?: PartialDeep<AppConfig>): AppConfig =>
 export const randomPort = () =>
   Math.floor(Math.random() * (65535 - 1024)) + 1024;
 
+export const ERR_UNEXPECTED_EXIT = 'Process exited unexpectedly';
+export const ERR_UNEXPECTED_EARLY_EXIT = 'Process exited early with code';
+
+export enum AppState {
+  RUNNING,
+  EXITED,
+}
+
 export class App {
+  private processState = AppState.RUNNING;
+
   constructor(
     private readonly process: ChildProcess,
     private readonly configPath: string,
-  ) {}
+  ) {
+    once(this.process, 'exit').then(() => {
+      this.processState = AppState.EXITED;
+    });
+  }
 
-  static async spawn(config?: AppConfig): Promise<App> {
+  static async spawn(config?: AppConfig, stableMs = 0): Promise<App> {
     const { path, cleanup } = await file({ postfix: '.json' });
 
     await writeFile(path, JSON.stringify(config ?? defaultConfig()));
@@ -53,12 +67,33 @@ export class App {
       '--config',
       path,
     ]);
-    return new App(appProcess, path);
+
+    return new Promise<App>((resolve, reject) => {
+      let exited = false;
+
+      appProcess.on('exit', (code) => {
+        exited = true;
+        cleanup();
+        reject(new Error(`Process exited early with code ${code}`));
+      });
+      appProcess.on('error', reject);
+
+      setTimeout(() => {
+        if (!exited) {
+          appProcess.removeAllListeners('exit');
+          resolve(new App(appProcess, path));
+        }
+      }, stableMs);
+    });
   }
 
   public async waitForReady(port?: number): Promise<App> {
     let times = 100;
     while (times-- > 0) {
+      // If the process exits while waiting for ready, this is considered an unexpected exit.
+      if (this.processState === AppState.EXITED)
+        throw new Error(ERR_UNEXPECTED_EXIT);
+
       try {
         await fetch(`http://localhost:${port ?? 3000}`);
         return this;
@@ -66,6 +101,10 @@ export class App {
       await new Promise((resolve) => setTimeout(resolve, 100));
     }
     throw new Error('Server failed to start');
+  }
+
+  public pid() {
+    return this.process.pid;
   }
 
   public async exit() {
