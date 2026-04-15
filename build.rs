c9207911fc799aa5ca64ff82457b0511f1a8e362
Zeping Bai <zeping@api7.ai>
2026-04-16 02:51:08 +0800

    feat(ui): add build-ui feature (#44)

--- build.rs
+++ build.rs
@@ -1,9 +1,10 @@
-use std::fs;
+use std::{env, fs, process};
 
+use anyhow::{Result, anyhow};
 use vergen_git2::{Emitter, Git2Builder};
 
-fn main() -> anyhow::Result<()> {
-    fs::create_dir_all("ui/dist").unwrap();
+fn main() -> Result<()> {
+    build_ui()?;
 
     Emitter::default()
         .add_instructions(&Git2Builder::default().sha(true).build()?)?
@@ -11,5 +12,25 @@ fn main() -> anyhow::Result<()> {
 
     println!("cargo:rerun-if-changed=build.rs");
 
+    Ok(())
+}
+
+fn build_ui() -> Result<()> {
+    if env::var("CARGO_FEATURE_BUILD_UI").is_ok() {
+        println!("cargo:rerun-if-changed=ui");
+
+        let status = process::Command::new("pnpm")
+            .args(["run", "build"])
+            .current_dir("ui")
+            .stdout(std::process::Stdio::inherit())
+            .stderr(std::process::Stdio::inherit())
+            .status()?;
+
+        if !status.success() {
+            return Err(anyhow!("failed to build ui with status: {}", status));
+        }
+    } else {
+        fs::create_dir_all("ui/dist")?;
+    }
     Ok(())
 }
