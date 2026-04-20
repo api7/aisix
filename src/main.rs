288abb02f6b2a87efc28c45523f7bf05077f2f32
AlinsRan <alinsran333@gmail.com>
2026-04-20 08:18:38 +0800

    Update src/main.rs
    Co-authored-by: Zeping Bai <zeping@api7.ai>

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,5 @@ use clap::Parser;
 #[tokio::main]
 async fn main() -> Result<()> {
     let args = Args::parse();
-    if let Err(e) = run(args.config).await {
-        eprintln!("Error: {e:#}");
-        exit(1);
-    }
-    Ok(())
+    run(args.config).await
 }
